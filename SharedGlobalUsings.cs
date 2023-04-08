ae00f97c40784b4cdab124c9ea35b7a1a4b2fb58
tgiachi <squid@stormwind.it>
2023-04-08 19:53:53 +0200

    feat: add SharedAssemblyInfo.cs, SharedGlobalUsings.cs, and stylecop.json files
    Add SharedAssemblyInfo.cs file to define assembly information, including version and company name.
    Add SharedGlobalUsings.cs file to define global usings for the project.
    Add stylecop.json file to configure StyleCopAnalyzers, including documentation rules.

---
+++ SharedGlobalUsings.cs
@@ -0,0 +1,15 @@
+// <copyright file="SharedGlobalUsings.cs" company="tgiachi">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+#pragma warning disable SA1200
+#pragma warning disable IDE0005
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+#pragma warning restore SA1200
+#pragma warning restore IDE0005
