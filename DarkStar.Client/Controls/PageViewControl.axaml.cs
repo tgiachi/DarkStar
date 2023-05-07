c861effda59220237e1525b60b87d138b9b58222
tgiachi <squid@stormwind.it>
2023-05-07 17:31:26 +0200

    feat(PageViewControl): add PageViewControl to MainWindow feat(MainWindow): add PageViewControl to MainWindow

---
+++ DarkStar.Client/Controls/PageViewControl.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DarkStar.Client.Controls;
+
+public partial class PageViewControl : UserControl
+{
+    public StyledProperty<Control> ControlProperty { get; set; } =   AvaloniaProperty.Register<PageViewControl, Control>(nameof(Control));
+    
+    public PageViewControl()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
